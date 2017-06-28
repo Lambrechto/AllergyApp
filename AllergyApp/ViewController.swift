5fef82bff49d25c95677ce891d0b1086006320b8
Lambrechto <daniellambrecht@macbook-pro.local>
2017-06-28 20:52:39 +0200

    Initial Commit

---
+++ AllergyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllergyApp
+//
+//  Created by Daniel Lambrecht on 28/06/2017.
+//  Copyright © 2017 Lambrecht Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
